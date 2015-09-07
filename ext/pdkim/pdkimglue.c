51247d951a9437ceb0f095e21403bcc03fb53d0c
Michael J. Welch, Ph.D <mike@czarmail.com>
2015-09-07 20:15:32 +0000

    Initial commit of v 0.1.

---
+++ ext/pdkim/pdkimglue.c
@@ -0,0 +1,227 @@
+#include <ruby.h>
+#include "pdkim1.h"
+#include <stdlib.h>
+
+/* These are, with the exception of 'object_ruby_pdkim_init_verify' and it's callback
+ * method 'query_dns_text', just glue routines for the C PDKIM library. */
+ 
+static VALUE pdkim_request_public_key = NULL;
+ 
+static VALUE object_ruby_pdkim_init_sign(VALUE rb_self, VALUE rb_mode, VALUE rb_domain, VALUE rb_selector, VALUE rb_rsa_privkey) {
+  int input_type = FIX2INT(rb_mode);
+  char* domain = StringValueCStr(rb_domain);
+  char* selector = StringValueCStr(rb_selector);
+  char* rsa_privkey = StringValueCStr(rb_rsa_privkey);
+  pdkim_ctx* ctx = pdkim_init_sign(
+    input_type, // Input type
+    domain,     // Domain
+    selector,   // Selector
+    rsa_privkey // Private RSA key
+  );
+  return LONG2FIX((long)ctx);
+}
+
+/* This callback activates the user's 'pdkim_init_verify' block to allow the user to lookup the domain's private key. */
+static int query_dns_text(char *name, char *answer) {
+  /* This mockup function was intended to encapulate the method to lookup
+   * the private key on the domain's webite, but Ruby uses a block on the
+   * 'pdkim_init_verify' call. This method is another glue routine. */
+  VALUE rb_answer = rb_funcall(pdkim_request_public_key, rb_intern("call"), 1, rb_str_new2(name));
+  strcpy(answer,StringValueCStr(rb_answer));
+  return PDKIM_OK;
+}
+
+static VALUE object_ruby_pdkim_init_verify(VALUE rb_self, VALUE rb_state) {
+  int state = FIX2INT(rb_state);
+  pdkim_ctx* ctx;
+  rb_need_block();
+  ctx = pdkim_init_verify(state, &query_dns_text);
+  // This is the block associated with the 'ruby_pdkim_init_verify' call
+  // in 'pdkim_init_verify' in 'pdkimlib' -- I store it in a global
+  // variable because there is no other way to get it to the callback method,
+  // which is defined here, but used in 'ruby_pdkim_feed_finish' -- I
+  // didn't want to modify the source code of the PDKIM itself
+  pdkim_request_public_key = rb_block_proc();
+  return LONG2FIX((long)ctx);
+}
+
+/* This method checks the argument for int or String, and uses that to open the debugging output file. */
+static VALUE object_ruby_pdkim_set_debug_stream(VALUE rb_self, VALUE rb_ctx, VALUE rb_file_id) {
+  pdkim_ctx* ctx = (pdkim_ctx*)FIX2LONG(rb_ctx);
+  FILE *debug;
+  if RB_TYPE_P(rb_file_id, T_STRING) {
+    debug = fopen(StringValueCStr(rb_file_id),"a");
+    if (debug==NULL) return Qfalse;
+    pdkim_set_debug_stream(ctx,debug);
+    return Qtrue;
+  }
+  else if RB_TYPE_P(rb_file_id, T_FIXNUM) {
+    debug = fdopen(FIX2SHORT(rb_file_id),"a");
+    pdkim_set_debug_stream(ctx,debug);
+    return Qtrue;
+  }
+  else return Qfalse;
+}
+
+// This method returns a NULL or a C String, depending on the object type
+static char* string_or_nil(VALUE obj) {
+  if (NIL_P(obj)) return NULL;
+  Check_Type(obj, T_STRING);
+  return StringValueCStr(obj);
+}
+
+//  Returns: 0 (PDKIM_OK) for success or a PDKIM_ERR_* constant
+static VALUE object_ruby_pdkim_set_optional(VALUE rb_self, VALUE rb_ctx, VALUE rb_sign_headers,
+    VALUE rb_identity, VALUE rb_canon_headers, VALUE rb_canon_body, VALUE rb_bodylength,
+    VALUE rb_algo, VALUE rb_created, VALUE rb_expires) {
+  pdkim_ctx* ctx = (pdkim_ctx*)FIX2LONG(rb_ctx);
+  char* sign_headers = string_or_nil(rb_sign_headers);
+  char* identity = string_or_nil(rb_identity);
+  int canon_headers = FIX2INT(rb_canon_headers);
+  int canon_body = FIX2INT(rb_canon_body);
+  long bodylength = FIX2LONG(rb_bodylength);
+  int algo = FIX2INT(rb_algo);
+  unsigned long created = FIX2LONG(rb_created);
+  unsigned long expires = FIX2LONG(rb_expires);
+  int ok = pdkim_set_optional(ctx, sign_headers, identity, canon_headers, canon_body, bodylength, algo, created, expires);
+
+  return LONG2FIX(ok);
+}
+
+static VALUE object_ruby_pdkim_feed(VALUE rb_self, VALUE rb_ctx, VALUE rb_data, VALUE rb_len) {
+  pdkim_ctx* ctx = (pdkim_ctx*)FIX2LONG(rb_ctx);
+  char* data = string_or_nil(rb_data);
+  long len = FIX2LONG(rb_len);
+  int ok = pdkim_feed(ctx, data, len);
+  return LONG2FIX(ok);
+}
+
+static void hashint(VALUE hash, const char* name, int value) {
+  rb_hash_aset(hash, ID2SYM(rb_intern(name)), INT2FIX(value));
+}
+
+static void hashlng(VALUE hash, const char* name, long value) {
+  rb_hash_aset(hash, ID2SYM(rb_intern(name)), LONG2FIX(value));
+}
+
+static void hashstr(VALUE hash, const char* name, char* value) {
+  if (value!=NULL) rb_hash_aset(hash, ID2SYM(rb_intern(name)), rb_str_export_locale((rb_str_new2(value))));
+  else rb_hash_aset(hash, ID2SYM(rb_intern(name)), Qnil);
+}
+
+static void hashbin(VALUE hash, const char* name, char* value, int len) {
+  if (len!=0) rb_hash_aset(hash, ID2SYM(rb_intern(name)), rb_str_export_locale((rb_str_new(value, len))));
+  else rb_hash_aset(hash, ID2SYM(rb_intern(name)), Qnil);
+}
+
+static VALUE object_ruby_pdkim_feed_finish(VALUE rb_self, VALUE rb_ctx) {
+  pdkim_ctx* ctx = (pdkim_ctx*)FIX2LONG(rb_ctx);
+  pdkim_signature *signature;
+  int ok = pdkim_feed_finish(ctx, &signature);
+  VALUE pubkey;
+
+  // Here, we create a hash from the return code and the signature, if any.
+  // Note that for a verify call, there may be many signatures in the email,
+  // and this will return them all.
+  VALUE pdkim_signatures = rb_ary_new();
+  while (signature!=NULL) {
+    VALUE pdkim_signature_hash = rb_hash_new();
+    hashint(pdkim_signature_hash, "error", ok);
+    hashstr(pdkim_signature_hash, "signature", signature->signature_header);
+    hashint(pdkim_signature_hash, "version", signature->version);
+    hashint(pdkim_signature_hash, "algo", signature->algo);
+    hashint(pdkim_signature_hash, "canon_headers", signature->canon_headers);
+    hashint(pdkim_signature_hash, "canon_body", signature->canon_body);
+    hashint(pdkim_signature_hash, "querymethod", signature->querymethod);
+    hashstr(pdkim_signature_hash, "selector", signature->selector);
+    hashstr(pdkim_signature_hash, "domain", signature->domain);
+    hashstr(pdkim_signature_hash, "identity", signature->identity);
+    hashlng(pdkim_signature_hash, "created", signature->created);
+    hashlng(pdkim_signature_hash, "expires", signature->expires);
+    hashlng(pdkim_signature_hash, "bodylength", signature->bodylength);
+    hashstr(pdkim_signature_hash, "headernames", signature->headernames);
+    hashstr(pdkim_signature_hash, "copiedheaders", signature->copiedheaders);
+    hashbin(pdkim_signature_hash, "sigdata", signature->sigdata, signature->sigdata_len);
+    hashbin(pdkim_signature_hash, "bodyhash", signature->bodyhash, signature->bodyhash_len);
+    hashstr(pdkim_signature_hash, "signature_header", signature->signature_header);
+    hashlng(pdkim_signature_hash, "verify_status", signature->verify_status);
+    hashlng(pdkim_signature_hash, "verify_ext_status", signature->verify_ext_status);
+
+    if (signature->pubkey!=NULL) {
+      pubkey = rb_hash_new();
+      hashstr(pubkey, "version", signature->pubkey->version);
+      hashstr(pubkey, "granularity", signature->pubkey->granularity);
+      hashstr(pubkey, "hashes", signature->pubkey->hashes);
+      hashstr(pubkey, "keytype", signature->pubkey->keytype);
+      hashstr(pubkey, "srvtype", signature->pubkey->srvtype);
+      hashstr(pubkey, "notes", signature->pubkey->notes);
+      hashbin(pubkey, "key", signature->pubkey->key, signature->pubkey->key_len);
+      hashint(pubkey, "testing", signature->pubkey->testing);
+      hashint(pubkey, "no_subdomaining", signature->pubkey->no_subdomaining);
+    } else pubkey = Qnil;
+    rb_hash_aset(pdkim_signature_hash, ID2SYM(rb_intern("pubkey")), pubkey);
+
+    rb_ary_push(pdkim_signatures, pdkim_signature_hash);
+    signature = signature->next;
+  }
+
+  return pdkim_signatures;
+}
+
+static VALUE object_ruby_pdkim_free_ctx(VALUE rb_self, VALUE rb_ctx) {
+  pdkim_free_ctx((pdkim_ctx*)FIX2LONG(rb_ctx));
+  return Qnil;
+}
+
+// Ruby setup
+void Init_pdkimglue() {
+  //Function success & error codes
+  rb_define_global_const("PDKIM_OK", LONG2FIX(PDKIM_OK));
+  rb_define_global_const("PDKIM_FAIL", LONG2FIX(PDKIM_FAIL));
+  rb_define_global_const("PDKIM_ERR_OOM", LONG2FIX(PDKIM_ERR_OOM));
+  rb_define_global_const("PDKIM_ERR_RSA_PRIVKEY", LONG2FIX(PDKIM_ERR_RSA_PRIVKEY));
+
+  rb_define_global_const("PDKIM_ERR_RSA_SIGNING", LONG2FIX(PDKIM_ERR_RSA_SIGNING));
+  rb_define_global_const("PDKIM_ERR_LONG_LINE", LONG2FIX(PDKIM_ERR_LONG_LINE));
+  rb_define_global_const("PDKIM_ERR_BUFFER_TOO_SMALL", LONG2FIX(PDKIM_ERR_BUFFER_TOO_SMALL));
+
+  // Context to keep state between all operations
+  rb_define_global_const("PDKIM_MODE_SIGN", LONG2FIX(PDKIM_MODE_SIGN));
+  rb_define_global_const("PDKIM_MODE_VERIFY", LONG2FIX(PDKIM_MODE_VERIFY));
+  rb_define_global_const("PDKIM_INPUT_NORMAL", LONG2FIX(PDKIM_INPUT_NORMAL));
+  rb_define_global_const("PDKIM_INPUT_SMTP", LONG2FIX(PDKIM_INPUT_SMTP));
+
+  // Canonicalizations
+  rb_define_global_const("PDKIM_CANON_SIMPLE", INT2FIX(PDKIM_CANON_SIMPLE));
+  rb_define_global_const("PDKIM_CANON_RELAXED", INT2FIX(PDKIM_CANON_RELAXED));
+
+  // Hash algorithms
+  rb_define_global_const("PDKIM_ALGO_RSA_SHA256", INT2FIX(PDKIM_ALGO_RSA_SHA256));
+  rb_define_global_const("PDKIM_ALGO_RSA_SHA1", INT2FIX(PDKIM_ALGO_RSA_SHA1));
+
+  // Hash algorithms
+  rb_define_global_const("PDKIM_HASH_SHA256", INT2FIX(PDKIM_HASH_SHA256));
+  rb_define_global_const("PDKIM_HASH_SHA1", INT2FIX(PDKIM_HASH_SHA1));
+
+  // Main verification status
+  rb_define_global_const("PDKIM_VERIFY_NONE", INT2FIX(PDKIM_VERIFY_NONE));
+  rb_define_global_const("PDKIM_VERIFY_INVALID", INT2FIX(PDKIM_VERIFY_INVALID));
+  rb_define_global_const("PDKIM_VERIFY_FAIL", INT2FIX(PDKIM_VERIFY_FAIL));
+  rb_define_global_const("PDKIM_VERIFY_PASS", INT2FIX(PDKIM_VERIFY_PASS));
+
+  // Extended verification status
+  rb_define_global_const("PDKIM_VERIFY_FAIL_BODY", INT2FIX(PDKIM_VERIFY_FAIL_BODY));
+  rb_define_global_const("PDKIM_VERIFY_FAIL_MESSAGE", INT2FIX(PDKIM_VERIFY_FAIL_MESSAGE));
+  rb_define_global_const("PDKIM_VERIFY_INVALID_PUBKEY_UNAVAILABLE", INT2FIX(PDKIM_VERIFY_INVALID_PUBKEY_UNAVAILABLE));
+  rb_define_global_const("PDKIM_VERIFY_INVALID_BUFFER_SIZE", INT2FIX(PDKIM_VERIFY_INVALID_BUFFER_SIZE));
+  rb_define_global_const("PDKIM_VERIFY_INVALID_PUBKEY_PARSING", INT2FIX(PDKIM_VERIFY_INVALID_PUBKEY_PARSING));
+
+  // Module definitions
+  rb_define_method(rb_cObject, "ruby_pdkim_init_sign", object_ruby_pdkim_init_sign, 4);
+  rb_define_method(rb_cObject, "ruby_pdkim_init_verify", object_ruby_pdkim_init_verify, 1);
+  rb_define_method(rb_cObject, "ruby_pdkim_set_debug_stream", object_ruby_pdkim_set_debug_stream, 2);
+  rb_define_method(rb_cObject, "ruby_pdkim_set_optional", object_ruby_pdkim_set_optional, 9);
+  rb_define_method(rb_cObject, "ruby_pdkim_feed", object_ruby_pdkim_feed, 3);
+  rb_define_method(rb_cObject, "ruby_pdkim_feed_finish", object_ruby_pdkim_feed_finish, 1);
+  rb_define_method(rb_cObject, "ruby_pdkim_free_ctx", object_ruby_pdkim_free_ctx, 1);
+}
---
+++ lib/pdkim.rb
@@ -0,0 +1,246 @@
+require_relative '../ext/pdkim/pdkimglue'
+
+module PDKIM
+
+  CRLF = "\r\n"
+
+  # ctx = pdkim_init_sign(mode, domain, selector, rsa_privkey)
+  #
+  # Initialize context for signing.
+  #
+  #    mode
+  #      PDKIM_INPUT_NORMAL or PDKIM_INPUT_SMTP. When SMTP
+  #      input is used, the lib will deflate double-dots at
+  #      the start of atline to a single dot, and it will
+  #      stop processing input when a line with and single
+  #      dot is received (Excess input will simply be ignored).
+  #
+  #    domain
+  #      The domain to sign as. This value will land in the
+  #      d= tag of the signature.
+  #
+  #    selector
+  #      The selector string to use. This value will land in
+  #      the s= tag of the signature.
+  #
+  #    rsa_privkey
+  #      The private RSA key, in ASCII armor. It MUST NOT be
+  #      encrypted.
+  #
+  # Returns: A freshly allocated ctx (context)
+  #
+  def pdkim_init_sign(mode, domain, selector, rsa_privkey)
+    ruby_pdkim_init_sign(mode, domain, selector, rsa_privkey)
+  end
+
+  # ctx = pdkim_init_verify(mode) { |name| ...code to retrieve domain's public key... }
+  #
+  # Initialize context for verification.
+  #
+  #    mode
+  #      PDKIM_INPUT_NORMAL or PDKIM_INPUT_SMTP. When SMTP
+  #      input is used, the lib will deflate double-dots at
+  #      the start of atline to a single dot, and it will
+  #      stop processing input when a line with and single
+  #      dot is received (Excess input will simply be ignored).
+  #
+  #    block
+  #      The lib does not include a DNS resolver, so you need
+  #      to provide that yourself. If you develop an application
+  #      that deals with email, you'll probably have something anyway.
+  #
+  # Returns: A freshly allocated ctx (context)
+  #
+  def pdkim_init_verify(state)
+    raise ArgumentError.new("pdkim_init_verify") if !block_given?
+    ruby_pdkim_init_verify(state) do |name|
+      r = yield(name)
+    end
+  end
+
+  # pdkim_set_debug_stream(ctx, file_name)
+  # pdkim_set_debug_stream(ctx, file_number)
+  #
+  # Set up debugging stream.
+  #
+  # When PDKIM was compiled with DEBUG defined (which is the
+  # recommended default), you can set up a stream where it
+  # sends debug output to. If you don't set a debug
+  # stream, no debug output is generated.
+  #
+  # Returns: nil
+  #
+  def pdkim_set_debug_stream(ctx, id)
+    ruby_pdkim_set_debug_stream(ctx, id)
+  end
+
+  # ok = pdkim_set_optional(ctx, sign_headers, identity, canon_headers, \
+  #                         canon_body, bodylength, algo, created, expires)
+  #
+  # OPTIONAL: Set additional optional signing options. If you do
+  # not use this function, sensible defaults (see below) are used.
+  # Any strings you pass in are dup'ed, so you can safely release
+  # your copy even before calling pdkim_free() on your context.
+  #
+  #    sign_headers (default nil)
+  #      Colon-separated list of header names. Headers with
+  #      a name matching the list will be included in the
+  #      signature. When this is NULL, the list of headers
+  #      recommended in RFC4781 will be used.
+  #
+  #    identity (default nil)
+  #      An identity string as described in RFC4781. It will
+  #      be put into the i= tag of the signature.
+  #
+  #    canon_headers (default PDKIM_CANON_SIMPLE)
+  #      Canonicalization algorithm to use for headers. One
+  #      of PDKIM_CANON_SIMPLE or PDKIM_CANON_RELAXED.
+  #
+  #    canon_body (default PDKIM_CANON_SIMPLE)
+  #      Canonicalization algorithm to use for the body. One
+  #      of PDKIM_CANON_SIMPLE or PDKIM_CANON_RELAXED.
+  #
+  #    bodylength (default -1)
+  #      Amount of canonicalized body bytes to include in
+  #      the body hash calculation. A value of 0 means that
+  #      the body is not included in the signature. A value
+  #      of -1 (the default) means that there is no limit.
+  #
+  #    algo (default PDKIM_ALGO_RSA_SHA256)
+  #      One of PDKIM_ALGO_RSA_SHA256 or PDKIM_ALGO_RSA_SHA1.
+  #
+  #    created (default 0)
+  #      Seconds since the epoch, describing when the signature
+  #      was created. This is copied to the t= tag of the
+  #      signature. Setting a value of 0 (the default) omits
+  #      the tag from the signature.
+  #
+  #    expires (default 0)
+  #      Seconds since the epoch, describing when the signature
+  #      expires. This is copied to the x= tag of the
+  #      signature. Setting a value of 0 (the default) omits
+  #      the tag from the signature.
+  #
+  #  Returns: 0 (PDKIM_OK) for success or a PDKIM_ERR_* constant
+  #
+  def pdkim_set_optional(ctx, sign_headers,
+      identity, canon_headers, canon_body, bodylength,
+      algo, created, expires)
+    ruby_pdkim_set_optional(ctx, sign_headers,
+      identity, canon_headers, canon_body, bodylength,
+      algo, created, expires)
+  end
+
+  # ok = pdkim_feed(ctx, data, data_len)
+  #
+  # (Repeatedly) feed data to the signing algorithm. The message
+  # data MUST use CRLF line endings (like SMTP uses on the
+  # wire). The data chunks do not need to be a "line" - you
+  # can split chunks at arbitrary locations.
+  #
+  #    data (Ruby String which is also allowed to contain binary)
+  #      Pointer to data to feed. Please note that despite
+  #      the example given below, this is not necessarily a
+  #      C string.
+  #
+  #    data_len
+  #      Length of data being fed, in bytes.
+  #
+  # Returns: 0 (PDKIM_OK) for success or a PDKIM_ERR_* constant
+  #
+  def pdkim_feed(ctx, data, len)
+    ruby_pdkim_feed(ctx, data, len)
+  end
+
+  # ok = pdkim_feed_finish(ctx)
+  #
+  # Signal end-of-message and retrieve the signature block.
+  #
+  #    pdkim_signature **signature
+  #      Pass in a pointer to a pdkim_signature pointer.
+  #      If the function returns PDKIM_OK, it will be set
+  #      up to point to a freshly allocated pdkim_signature
+  #      block. See pdkim.h for documentation on what that
+  #      block contains. Hint: Most implementations will
+  #      simply want to retrieve a ready-to-use
+  #      DKIM-Signature header, which can be found in
+  #      *signature->signature_header. See the code below.
+  #
+  # Returns: An array of hashes (only 1 for sign) with the signatures in them
+  #
+  #[
+  #  {
+  #    "error"=>0, # 0 (PDKIM_OK) for success or a PDKIM_ERR_* constant
+  #    "signature"=>nil,
+  #    "version"=>1,
+  #    "algo"=>0,
+  #    "canon_headers"=>0,
+  #    "canon_body"=>0,
+  #    "querymethod"=>0,
+  #    "selector"=>"cheezburger",
+  #    "domain"=>"duncanthrax.net",
+  #    "identity"=>nil,
+  #    "created"=>0,
+  #    "expires"=>0,
+  #    "bodylength"=>-1,
+  #    "headernames"=>"Subject:To:From",
+  #    "copiedheaders"=>nil,
+  #    "sigdata"=>"\xA1\xEDy\x16\xDF\xF1\xF8C\x18\x80\xF8\x1F@\xFCIV&\x0E\xA4\xD5 ...",
+  #    "bodyhash"=>"M\x87\xE3_\xE5;T\xD4\x96\x90'I\xEA2\xBF\xCE\x8F\x17\xCD\xEF ...",
+  #    "signature_header"=>nil,
+  #    "verify_status"=>3,
+  #    "verify_ext_status"=>0,
+  #    "pubkey"=>{
+  #      "version"=>"DKIM1",
+  #      "granularity"=>"*",
+  #      "hashes"=>nil,
+  #      "keytype"=>"rsa",
+  #      "srvtype"=>"*",
+  #      "notes"=>nil,
+  #      "key"=>"0\x81\x9F0\r\x06\t*\x86H\x86\xF7\r\x01\x01\x01\x05\x00\x03\x81\x8D ...",
+  #      "testing"=>0,
+  #      "no_subdomaining"=>0
+  #    }
+  #  }
+  #]
+  def pdkim_feed_finish(ctx)
+    ruby_pdkim_feed_finish(ctx)
+  end
+
+  # pdkim_free_ctx(ctx)
+  #
+  #  Free all allocated memory blocks referenced from
+  #  the context, as well as the context itself.
+  #
+  #  Don't forget to call this or your application will "leak" memory.
+  #
+  def pdkim_free_ctx(ctx)
+    ruby_pdkim_free_ctx(ctx)
+  end
+
+  def pdkim_sign_an_email(mode, domain, selector, rsa_privkey, canon_headers, canon_body, message)
+    ctx = pdkim_init_sign(mode, domain, selector, rsa_privkey)
+    ok = pdkim_set_optional(ctx, nil, nil, canon_headers, canon_body, -1, PDKIM_ALGO_RSA_SHA256, 0, 0)
+    message.each do |line| 
+      ok = pdkim_feed(ctx, line, line.length)
+    end
+    signatures = pdkim_feed_finish(ctx)
+    email = signatures[0][:signature] + CRLF + message.join("")
+    pdkim_free_ctx(ctx)
+    return email
+  end
+
+  def pdkim_verify_an_email(mode, email, sym_domain_lookup)
+    ctx = pdkim_init_verify(mode) do |name|
+      send(sym_domain_lookup, name)
+    end
+    ok = pdkim_feed(ctx, email, email.length)
+    signatures = pdkim_feed_finish(ctx)
+    verify_counts = [0, 0, 0, 0]
+    signatures.each do |signature|
+      verify_counts[signature[:verify_status]] += 1
+    end
+    pdkim_free_ctx(ctx)
+    return verify_counts
+  end
+end
